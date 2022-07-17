1fe3f510c33151edb3ef5c647db26662e27a0a7c
Yassin Bahri <55001682+student1043@users.noreply.github.com>
2022-07-17 19:56:43 +0100

    Update streamlitfilter.py

--- streamlitfilter.py
+++ streamlitfilter.py
@@ -10,7 +10,6 @@ class Generator():
     tokenizerfile.pad_token = tokenizerfile.eos_token
     gradient_ckpt = True
     model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M", pad_token_id=tokenizerfile.eos_token_id, gradient_checkpointing=gradient_ckpt, use_cache=not gradient_ckpt)
-    parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False)
     def test_generate(self, input_str: str):
         input_ids = tokenizerfile.encode(input_str, add_special_tokens=False, return_tensors="pt")
         attention_mask = torch.where(input_ids == tokenizerfile.eos_token_id, torch.zeros_like(input_ids), torch.ones_like(input_ids)).to(model.device)
@@ -19,6 +18,12 @@ class Generator():
         print(output_str)
 
 
-para_phrases = "what happened in world war 2"
+parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False)
+phrases = []
+phrases.append(input("insert phrase to search: "))
+print(phrases)
+for phrase in phrases:
+    para_phrases = parrot.augment(input_phrase=phrase, use_gpu=False)
+    print(para_phrases)
 
-sa = Generator().test_generate(para_phrases)
+sa = Generator().test_generate(para_phrases[0])
