501d076e42d998ef0c63221a695dd1741797367a
Yassin Bahri <55001682+student1043@users.noreply.github.com>
2022-07-17 19:41:28 +0100

    Update streamlitfilter.py

--- streamlitfilter.py
+++ streamlitfilter.py
@@ -11,7 +11,7 @@ gradient_ckpt = True
 model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M", pad_token_id=tokenizerfile.eos_token_id, gradient_checkpointing=gradient_ckpt, use_cache=not gradient_ckpt)
 parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False)
 phrases = []
-phrases.append(input("insert phrase to search"))
+phrases.append(input("insert phrase to search: "))
 print(phrases)
 
 def test_generate(input_str: str):
@@ -24,5 +24,4 @@ def test_generate(input_str: str):
 for phrase in phrases:
     para_phrases = parrot.augment(input_phrase=phrase, use_gpu=False)
     print(para_phrases)
-    selected_items = [item[0] for item in para_phrases]
-test_generate(selected_items[0])
+test_generate(para_phrases[0])
