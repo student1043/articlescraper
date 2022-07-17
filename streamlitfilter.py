0e2ed84c1646928d49fc90185f554e3b8e30dfbd
Yassin Bahri <55001682+student1043@users.noreply.github.com>
2022-07-17 20:02:22 +0100

    Update streamlitfilter.py

--- streamlitfilter.py
+++ streamlitfilter.py
@@ -6,11 +6,11 @@ import streamlit as st
 
 
 class Generator():
-    tokenizerfile = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
-    tokenizerfile.pad_token = tokenizerfile.eos_token
-    gradient_ckpt = True
-    model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M", pad_token_id=tokenizerfile.eos_token_id, gradient_checkpointing=gradient_ckpt, use_cache=not gradient_ckpt)
     def test_generate(self, input_str: str):
+        tokenizerfile = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
+        tokenizerfile.pad_token = tokenizerfile.eos_token
+        gradient_ckpt = True
+        model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M", pad_token_id=tokenizerfile.eos_token_id, gradient_checkpointing=gradient_ckpt, use_cache=not gradient_ckpt)
         input_ids = tokenizerfile.encode(input_str, add_special_tokens=False, return_tensors="pt")
         attention_mask = torch.where(input_ids == tokenizerfile.eos_token_id, torch.zeros_like(input_ids), torch.ones_like(input_ids)).to(model.device)
         output_ids = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=30, num_return_sequences=1, min_length=500, max_length=750)
