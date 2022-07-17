d4227b939df398c9493c4fa12446965858feecf3
Yassin Bahri <55001682+student1043@users.noreply.github.com>
2022-07-17 20:16:43 +0100

    Update streamlitfilter.py

--- streamlitfilter.py
+++ streamlitfilter.py
@@ -13,7 +13,7 @@ class Generator():
         model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M", pad_token_id=tokenizerfile.eos_token_id, gradient_checkpointing=gradient_ckpt, use_cache=not gradient_ckpt)
         input_ids = tokenizerfile.encode(input_str, add_special_tokens=False, return_tensors="pt")
         attention_mask = torch.where(input_ids == tokenizerfile.eos_token_id, torch.zeros_like(input_ids), torch.ones_like(input_ids)).to(model.device)
-        output_ids = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=30, num_return_sequences=1, max_length=750)
+        output_ids = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=750, num_return_sequences=1)
         output_str = tokenizerfile.decode(output_ids[0], skip_special_tokens=False, clean_up_tokenization_spaces=False)
         print(output_str)
 
