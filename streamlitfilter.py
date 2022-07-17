47628946a1976be81fff0bd1d4ddda15b0787cfa
Yassin Bahri <55001682+student1043@users.noreply.github.com>
2022-07-17 21:53:16 +0100

    Update streamlitfilter.py

--- streamlitfilter.py
+++ streamlitfilter.py
@@ -2,7 +2,6 @@ from parrot import Parrot
 from transformers import pipeline, GPTNeoForCausalLM, GPT2Tokenizer
 import torch
 import warnings
-import streamlit as st
 
 
 class Generator():
@@ -15,18 +14,19 @@ class Generator():
         attention_mask = torch.where(input_ids == tokenizerfile.eos_token_id, torch.zeros_like(input_ids), torch.ones_like(input_ids)).to(model.device)
         output_ids = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=750, num_return_sequences=1)
         output_str = tokenizerfile.decode(output_ids[0], skip_special_tokens=False, clean_up_tokenization_spaces=False)
-        print(output_str)
-
-    def parrot_generate(text):
-        parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False)
-        phrases = []
-        phrases.append(text)
-        print(phrases)
-        for phrase in phrases:
-            para_phrases = []
-            para_phrases = parrot.augment(input_phrase=phrase, use_gpu=False)
-            for p in para_phrases:
-                selected = p[0]
-            return(selected)
+        return(output_str)
 
 
+parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False)
+phrases = []
+phrases.append(input("insert phrase to search: "))
+put_text(phrases)
+for phrase in phrases:
+    para_phrases = []
+    para_phrases = parrot.augment(input_phrase=phrase, use_gpu=False)
+    for p in para_phrases:
+        selected = p[0]
+    return(selected)
+
+sa = Generator().test_generate(selected)
+print(sa)
