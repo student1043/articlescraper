66c24f545c2db905d9af5e52604ec8fc94551205
Yassin Bahri <55001682+student1043@users.noreply.github.com>
2022-07-17 23:48:56 +0100

    Update and rename gunicorn_config.py to frontend.py

---
+++ frontend.py
@@ -0,0 +1,8 @@
+import multiprocessing
+import streamlit as st
+from app import *
+
+
+st.title("ARTICLE GENERATOR")
+text = st.text_input("Text", value="")
+t = multiprocessing.Process(target=Generator, args=(text))
