7ce647d1351ebc778efd201b0e1e32d4b1612196
student1043 <1043@holbertonschool.com>
2022-07-17 18:16:34 +0100

    test

--- streamlitfilter.py
+++ streamlitfilter.py
@@ -4,11 +4,11 @@ import torch
 import warnings
 import streamlit as st
 
-@st.cache
-tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
-tokenizer.pad_token = tokenizer.eos_token
+
+tokenizerfile = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
+tokenizerfile.pad_token = tokenizerfile.eos_token
 gradient_ckpt = True
-model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M", pad_token_id=tokenizer.eos_token_id, gradient_checkpointing=gradient_ckpt, use_cache=not gradient_ckpt)
+model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M", pad_token_id=tokenizerfile.eos_token_id, gradient_checkpointing=gradient_ckpt, use_cache=not gradient_ckpt)
 parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False)
 phrases = []
 phrases.append(st.text_input("insert phrase to search", ""))
@@ -16,10 +16,10 @@ phrases.append(st.text_input("insert phrase to search", ""))
 
 @st.cache
 def test_generate(input_str: str):
-    input_ids = tokenizer.encode(input_str, add_special_tokens=False, return_tensors="pt")
-    attention_mask = torch.where(input_ids == tokenizer.eos_token_id, torch.zeros_like(input_ids), torch.ones_like(input_ids)).to(model.device)
+    input_ids = tokenizerfile.encode(input_str, add_special_tokens=False, return_tensors="pt")
+    attention_mask = torch.where(input_ids == tokenizerfile.eos_token_id, torch.zeros_like(input_ids), torch.ones_like(input_ids)).to(model.device)
     output_ids = model.generate(input_ids, attention_mask=attention_mask, max_new_tokens=30, num_return_sequences=1, min_length=500, max_length=750)
-    output_str = tokenizer.decode(output_ids[0], skip_special_tokens=False, clean_up_tokenization_spaces=False)
+    output_str = tokenizerfile.decode(output_ids[0], skip_special_tokens=False, clean_up_tokenization_spaces=False)
     st.write(output_str)
 
 for phrase in phrases:
